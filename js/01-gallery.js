a250ea0c87cca881281fb6f9ad04d73c83fbc3d4
AnimeMakartur <shishkov.egor4a@gmail.com>
2025-02-26 17:44:32 +0200

    243

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,113 +1,107 @@
 import { galleryItems } from './gallery-items.js';
 
-const refs = {
-  galleryListEl: document.querySelector('ul.gallery'),
-  modalContainerEl: document.querySelector('div.lightbox'),
-  lightboxOverlayEl: document.querySelector('div.lightbox__overlay'),
-  modalImgEl: document.querySelector('img.lightbox__image'),
-  modalCloseBtnEl: document.querySelector('[data-action="close-lightbox"]'),
-};
-
-const galleryMarkup = createGalleryMarkup(galleryItems);
-refs.galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
-refs.galleryListEl.addEventListener('click', onGalleryItemClick);
-refs.modalCloseBtnEl.addEventListener('click', closeModal);
-refs.lightboxOverlayEl.addEventListener('click', closeModal); // 7.
-
 function createGalleryMarkup(galleryItems) {
-  return galleryItems
-      .map(({preview, original, description}) => {
-          return `<li class="gallery__item">
-                  <a class="gallery__link" href="${original}">
-                      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
-                  </a>
-              </li>`;
-      })
-      .join('');
+    return galleryItems
+        .map(({preview, original, description}) => {
+            return `<li class="gallery__item">
+                    <a class="gallery__link" href="${original}">
+                        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+                    </a>
+                </li>`;
+        })
+        .join('');
 }
 
 
 function onGalleryItemClick(event) {
-  event.preventDefault();
+    // коли клікаємо на img, відмінюємо по замовчуванню перезавантаження сторінки прівент
+    // діє лише на подію(event/e)
+    event.preventDefault();
 
+    // 3. Відкриття модального вікна по кліці на елементі галереї.
 
-  const isGalleryEl = event
-      .target
-      .classList
-      .contains('gallery__image');
-  if (!isGalleryEl) {
-      return;
-  }
-  refs
-      .modalContainerEl
-      .classList
-      .add('is-open');
+    const isGalleryEl = event
+        .target
+        .classList
+        .contains('gallery__image');
+    if (!isGalleryEl) {
+        return;
+    }
+    refs
+        .modalContainerEl
+        .classList
+        .add('is-open');
 
-  refs.modalImgEl.src = event.target.dataset.source;
-  refs.modalImgEl.alt = event
-      .target
-      .getAttribute('alt');
+    // 4. Підміна значення атрибута src елемента img.lightbox__image.
 
-  document.addEventListener('keydown', onEscPress);
-  document.addEventListener('keydown', onArrowPress);
+    refs.modalImgEl.src = event.target.dataset.source;
+    refs.modalImgEl.alt = event
+        .target
+        .getAttribute('alt');
+
+    document.addEventListener('keydown', onEscPress);
+    document.addEventListener('keydown', onArrowPress);
 
 }
 
+// 5. Закриття модального вікна по кліці  на кнопку 
+// button[data-action="close-lightbox"]
 
 function closeModal() {
-  refs
-      .modalContainerEl
-      .classList
-      .remove('is-open');
+    refs
+        .modalContainerEl
+        .classList
+        .remove('is-open');
 
-  refs.modalImgEl.src = '';
-  refs.modalImgEl.alt = '';
+    // 6. Очищення значення атрибута src елемента img.lightbox__image.
+    refs.modalImgEl.src = '';
+    refs.modalImgEl.alt = '';
 
-  document.removeEventListener('keydown', onEscPress);
-  document.removeEventListener('keydown', onArrowPress);
+    document.removeEventListener('keydown', onEscPress);
+    document.removeEventListener('keydown', onArrowPress);
 }
 
 refs.lightboxOverlayEl.addEventListener('click', closeModal);
 
 function onEscPress(event) {
-  if (event.key === 'Escape') {
-      closeModal();
-  }
+    if (event.key === 'Escape') {
+        closeModal();
+    }
 }
 refs.lightboxOverlayEl.addEventListener('click', closeModal);
 
 const turnRight = currentIndex => {
-  if (currentIndex === galleryItems.length - 1) 
-      return;
-  const nextImg = galleryItems[currentIndex + 1].original;
-  refs
-      .modalImgEl
-      .setAttribute('src', nextImg);
+    if (currentIndex === galleryItems.length - 1) 
+        return;
+    const nextImg = galleryItems[currentIndex + 1].original;
+    refs
+        .modalImgEl
+        .setAttribute('src', nextImg);
 };
 
 const turnfLeft = currentIndex => {
-  if (currentIndex === 0) 
-      return;
-  const previousImg = galleryItems[currentIndex - 1].original;
-  refs
-      .modalImgEl
-      .setAttribute('src', previousImg);
+    if (currentIndex === 0) 
+        return;
+    const previousImg = galleryItems[currentIndex - 1].original;
+    refs
+        .modalImgEl
+        .setAttribute('src', previousImg);
 };
 
-
 function onArrowPress(event) {
-  if (refs.modalContainerEl.classList.contains('is-open')) {
-      const currentImg = refs
-          .modalImgEl
-          .getAttribute('src');
-      const currentIndex = galleryItems.indexOf(
-          galleryItems.find(item => item.original === currentImg),
-      );
-      if (event.code === 'ArrowRight') 
-          turnRight(currentIndex);
-      if (event.code === 'ArrowLeft') 
-          turnfLeft(currentIndex);
-      }
-  }
+    if (refs.modalContainerEl.classList.contains('is-open')) {
+        const currentImg = refs
+            .modalImgEl
+            .getAttribute('src');
+        const currentIndex = galleryItems.indexOf(
+            galleryItems.find(item => item.original === currentImg),
+        );
+        if (event.code === 'ArrowRight') 
+            turnRight(currentIndex);
+        if (event.code === 'ArrowLeft') 
+            turnfLeft(currentIndex);
+        }
+    }
+
 
 console.log(galleryItems);
