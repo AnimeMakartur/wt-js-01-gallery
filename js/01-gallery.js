8fdd78e6afb68ca2a3503c54dd6b532326ffc52e
AnimeMakartur <shishkov.egor4a@gmail.com>
2025-02-26 12:14:04 +0200

    32432

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 
+const refs = {
+  galleryListEl: document.querySelector('ul.gallery'),
+  modalContainerEl: document.querySelector('div.lightbox'),
+  lightboxOverlayEl: document.querySelector('div.lightbox__overlay'),
+  modalImgEl: document.querySelector('img.lightbox__image'),
+  modalCloseBtnEl: document.querySelector('[data-action="close-lightbox"]'),
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+refs.galleryListEl.insertAdjacentHTML('beforeend', galleryMarkup);
+refs.galleryListEl.addEventListener('click', onGalleryItemClick);
+refs.modalCloseBtnEl.addEventListener('click', closeModal);
+refs.lightboxOverlayEl.addEventListener('click', closeModal); // 7.
+
 function createGalleryMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
